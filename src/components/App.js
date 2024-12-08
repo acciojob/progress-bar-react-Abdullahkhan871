16bf49592e57bac5abbe6fed4bb2b0409a39e807
Abdullah khan <akhan283785@gmail.com>
2024-12-08 10:11:13 +0530

    first try

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState, useEffect } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [bar, setBar] = useState(0);
+
+  useEffect(() => {
+    // let inter;
+    if (bar != 100) {
+      let inter = setTimeout(() => {
+        setBar((pre) => pre + 10);
+      }, 1000);
+      return () => setInterval(inter);
+    }
+    // return () => setInterval(inter);
+  }, [bar]);
+
+  let outerBar = {
+    width: "50vh",
+    height: "20px",
+    backgroundColor: "gray",
+  };
+
+  let innerBar = {
+    width: `${bar}%`,
+    height: "100%",
+    backgroundColor: "blue",
+  };
   return (
     <div>
-        {/* Do not remove the main div */}
+      <h1>Progress Bar</h1>
+      <div className="barOuter" style={outerBar}>
+        <div className="barInner" style={innerBar}></div>
+      </div>
+      <p>{bar}%</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
